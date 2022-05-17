49c085ccdc8b374c29ec275cdf559b532ca89cc3
Omer Berger <omermberger@gmail.com>
2022-05-17 17:48:53 -0400

    Initial Commit

---
+++ Cocktail_Pantry/Cocktail_PantryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cocktail_PantryApp.swift
+//  Cocktail_Pantry
+//
+//  Created by Omer Berger on 5/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Cocktail_PantryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
