d51a9b84b6cd785180cda915ac910051b9681e89
Omer Berger <omermberger@gmail.com>
2023-01-04 13:12:33 -0500

    Accidently omitted in previous commit - new file for ExploreCard

---
+++ Cocktail_Pantry/Views/Tab Views/Explore Tab Views/ExploreCard.swift
@@ -0,0 +1,26 @@
+//
+//  ExploreCard.swift
+//  Cocktail_Pantry
+//
+//  Created by Omer Berger on 1/3/23.
+//
+
+import SwiftUI
+
+struct ExploreCard: View {
+    var text: String
+    var imageName: String
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 0).foregroundColor(.white).frame(width: 130, height: 100)
+            VStack {
+                Image(imageName)
+                    .resizable().scaledToFill().frame(width: 130, height: 100).clipped()
+                Text(text).padding(.bottom, 8).foregroundColor(.black).frame(height: 30)
+            }
+            .frame(width: 130, height: 130)
+            RoundedRectangle(cornerRadius: 0).stroke(lineWidth: 1.5).foregroundColor(.gray).frame(width: 130, height: 130)
+        }
+    }
+}
