ad7043485ff8c511c54c537b3d2cda73bd5a74e1
Omer Berger <84885120+AnOmerBerger@users.noreply.github.com>
2023-01-18 21:12:01 +0200

    Update HomeTab.swift

--- Cocktail_Pantry/Views/Tab Views/Home Tab Views/HomeTab.swift
+++ Cocktail_Pantry/Views/Tab Views/Home Tab Views/HomeTab.swift
@@ -23,32 +23,5 @@ struct HomeTab: View {
                 SimpleSearchPage(searchText: $cocktaileSearchText).environmentObject(viewModel)
             }
         }
-    }
-    
-    
-    
-    
-//    struct filterToggle: View {
-//        @Binding var isOn: Bool
-//
-//        var body: some View {
-//            ZStack {
-//                RoundedRectangle(cornerRadius: 10).foregroundColor(.gray)
-//                    .frame(width: 160, height: 40)
-//                RoundedRectangle(cornerRadius: 0).foregroundColor(.white)
-//                    .frame(width: 150, height: 30)
-//                    .overlay {
-//                        Toggle(isOn: $isOn) {
-//                            HStack {
-//                                Image(systemName: "slider.vertical.3").imageScale(.small)
-//                                Text(isOn ? "pantry mode" : "simple search").font(.caption)
-//                            }
-//                        }
-//                        .toggleStyle(.button)
-//                        .padding()
-//                    }
-//            }
-//            .padding(.horizontal)
-//        }
-//    }
+    }    
 }
